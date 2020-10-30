4a09189e5619d37dc71ef3d64666fb2161a6caca
Vlad Shcherban <vlad@coronalabs.com>
2020-10-30 15:34:42 +0200

    Deleting old code and adding checks.

--- src/android/plugin/src/main/java/plugin/pasteboard/ClipboardListener.java
+++ src/android/plugin/src/main/java/plugin/pasteboard/ClipboardListener.java
@@ -8,157 +8,77 @@
 // Package name
 package plugin.pasteboard;
 
-// Java Imports
-import java.util.*;
-import java.lang.*;
-
-// Android Imports
 import android.content.Context;
 
-// Corona Imports
 import com.ansca.corona.CoronaActivity;
 import com.ansca.corona.CoronaEnvironment;
 
 
 // Clipboard listener class
-public class ClipboardListener
-{
-	// Private vars
-	private Timer timer;
+public class ClipboardListener {
+    private android.content.ClipboardManager clipboardManager;
+    private android.content.ClipboardManager.OnPrimaryClipChangedListener primaryClipChangedListener;
 
-	// Functions for API Level 11 and above
-	private static class ApiLevel11
-	{
-		/** Constructor made private to prevent instances from being made. */
-		private ApiLevel11() { }
+    private void setNewPasteboardItem(Context context) {
 
-		// Private vars
-		private static android.content.ClipboardManager clipboardManager;
-		private static android.content.ClipboardManager.OnPrimaryClipChangedListener primaryClipChangedListener;
+        if (clipboardManager == null) {
+            clipboardManager = (android.content.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        }
 
-		// Function to add the clipChanged listener
-		public static boolean addClipChangedListener( final Context context )
-		{
-			// Verify environment
-			CoronaActivity coronaActivity = CoronaEnvironment.getCoronaActivity();
-			if ( coronaActivity == null ) { return false; }
-			
-			// Create a new runnable object to invoke our activity
-			Runnable activityRunnable = new Runnable()
-			{
-				public void run()
-				{
-					// Grab the initial clipboard contents and put them in pasteboard, if any.
-					setNewPasteboardItem( context );
-
-					// Clip changed listener
-					primaryClipChangedListener = new android.content.ClipboardManager.OnPrimaryClipChangedListener()
-					{
-						public void onPrimaryClipChanged()
-						{
-							setNewPasteboardItem( context );
-						}
-					};
-
-					// Add the clip listener
-					clipboardManager.addPrimaryClipChangedListener( primaryClipChangedListener );
-				}
-			};
-
-			// Run the activity on the uiThread
-			coronaActivity.runOnUiThread( activityRunnable );
-
-			return true;
-		}
-
-		// Function to remove the clipChanged listener
-		public static boolean removeClipChangedListener()
-		{
-			// Remove the clip listener
-			clipboardManager.removePrimaryClipChangedListener( primaryClipChangedListener );
-			return true;
-		}
-
-		// Function to set a new pasteboard item.
-		// Can be called from lua thread or UI thread.
-		private static void setNewPasteboardItem( Context context ) {
-
-			// Assign the clipboard manager
-			clipboardManager = ( android.content.ClipboardManager )context.getSystemService( context.CLIPBOARD_SERVICE );
-
-			// If the Clipboard contains data
-			if ( clipboardManager.hasPrimaryClip() )
-			{
-				// Get the primary clip
-				android.content.ClipData clipData = clipboardManager.getPrimaryClip();
-
-				// Set the clipdata item
-				android.content.ClipData.Item item = clipData.getItemAt( 0 );
-
-				// Set the currentPasteboard item to the new text.
-				shared.setCurrentPasteboardItem(shared.ApiLevel11.coerceToString(context, item));
-			}
-		}
-	}
+        // If the Clipboard contains data, save it to shared
+        if (clipboardManager.hasPrimaryClip()) {
+            android.content.ClipData clipData = clipboardManager.getPrimaryClip();
+            android.content.ClipData.Item item = clipData.getItemAt(0);
+            shared.setCurrentPasteboardItem(shared.ApiLevel11.coerceToString(context, item));
+        }
+    }
 
 
+    // Function to add the clipboard listener
+    public void addClipChangedListener() {
+        // Verify environment
+        final Context context = CoronaEnvironment.getApplicationContext();
+        if (context == null) return;
 
-	// Function to add the clipboard listener
-	public boolean addClipChangedListener()
-	{
-		// Verify environment
-		Context context = CoronaEnvironment.getApplicationContext();
-		if ( context == null ) { return false; }
+        if (clipboardManager == null) {
+            clipboardManager = (android.content.ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        }
+        if (clipboardManager == null) return;
 
-		// Api levels above or equal to 11
-		if ( android.os.Build.VERSION.SDK_INT >= 11 )
-		{
-			ApiLevel11.addClipChangedListener( context );
-		}
-		// Api's older than 11
-		else
-		{
-			// Create a new timer
-			timer = new Timer();
-			// Setup a Clipboard manager instance
-			final android.text.ClipboardManager clipboardManager;
-			clipboardManager = ( android.text.ClipboardManager )context.getSystemService( Context.CLIPBOARD_SERVICE );
-			// Set the currentPasteboard item to the new text
-			shared.setCurrentPasteboardItem(clipboardManager.getText().toString());
+        // Verify environment
+        CoronaActivity coronaActivity = CoronaEnvironment.getCoronaActivity();
+        if (coronaActivity == null) return;
 
-			// Start the timer
-			timer.scheduleAtFixedRate( new java.util.TimerTask()
-			{
-				@Override
-				public void run()
-				{
-					// Set the currentPasteboard item to the new text
-					shared.setCurrentPasteboardItem(clipboardManager.getText().toString());
-				}
-			}, 0, 100 );
-		}
+        if (primaryClipChangedListener == null) {
+            primaryClipChangedListener = new android.content.ClipboardManager.OnPrimaryClipChangedListener() {
+                public void onPrimaryClipChanged() {
+                    setNewPasteboardItem(context);
+                }
+            };
+        }
 
-		return true;
-	}
+        coronaActivity.runOnUiThread(new Runnable() {
+            public void run() {
+                // Grab the initial clipboard contents and put them in pasteboard, if any.
+                setNewPasteboardItem(context);
+                clipboardManager.addPrimaryClipChangedListener(primaryClipChangedListener);
+            }
+        });
+    }
 
-	// Function to remove the clipboard listener
-	public boolean removeClipChangedListener()
-	{
-		// Verify environment
-		Context context = CoronaEnvironment.getApplicationContext();
-		if ( context == null ) { return false; }
+    // Function to remove the clipboard listener
+    public void removeClipChangedListener() {
+        // Verify environment
+        Context context = CoronaEnvironment.getApplicationContext();
+        if (context == null) return;
 
-		// Api levels above or equal to 11
-		if ( android.os.Build.VERSION.SDK_INT >= 11 )
-		{
-			ApiLevel11.removeClipChangedListener();
-		}
-		// Api's older than 11
-		else
-		{
-			timer.cancel();
-		}
-
-		return true;
-	}
+        // Remove the clip listener
+        if (clipboardManager == null || primaryClipChangedListener == null) {
+            return;
+        }
+        try {
+            clipboardManager.removePrimaryClipChangedListener(primaryClipChangedListener);
+        } catch (Throwable ignore) {
+        }
+    }
 }
